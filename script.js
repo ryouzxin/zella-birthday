b40a2d00c3b6acf740dd4ee557c4373f0d39d515
Tarisa <ryouzxin@gmail.com>
2026-07-05 07:17:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('click',e=>{
+ const r=document.createElement('div');
+ r.className='ripple';
+ r.style.left=e.clientX+'px';
+ r.style.top=e.clientY+'px';
+ document.body.appendChild(r);
+ setTimeout(()=>r.remove(),1000);
+});
