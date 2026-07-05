80b81cda58b6fbe47e9e48aea443acb136b5926a
Tarisa <ryouzxin@gmail.com>
2026-07-05 09:20:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,30 +8,50 @@ const popup = document.getElementById("popup");
 let musicStarted = false;
 
 function showPopup(text){
+
     popup.innerText = text;
+
     popup.style.top = "20px";
 
     setTimeout(()=>{
         popup.style.top = "-100px";
-    },2000);
+    },2500);
+
 }
 
 btn.addEventListener("click",()=>{
 
+    btn.classList.add("bounce");
+
+    setTimeout(()=>{
+        btn.classList.remove("bounce");
+    },450);
+
+    // klik pertama
     if(clickCount === 0){
+
         showPopup("🤭 Eits, belum dulu dong~");
+
         clickCount++;
+
     }
 
+    // klik kedua
     else if(clickCount === 1){
 
         if(!musicStarted){
+
             showPopup("🎵 Sebelum lanjut, nyalain musik dulu ya~");
-            musicBtn.style.display="block";
+
+            musicBtn.style.display = "block";
+
         }
         else{
+
             window.location.href="puzzle.html";
+
         }
+
     }
 
 });
@@ -42,7 +62,9 @@ musicBtn.addEventListener("click",()=>{
 
     musicStarted = true;
 
-    musicBtn.style.display="none";
+    musicBtn.style.display = "none";
+
+    btn.innerHTML = "🧩 Mulai Puzzle";
 
     showPopup("✨ Nah gitu dong~");
 
