42f22b1859c9a284d4ff9cf6cb7886b4dd3bb182
Tarisa <ryouzxin@gmail.com>
2026-07-05 07:47:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,61 @@
 let clickCount = 0;
+
 const btn = document.getElementById("openBtn");
 const message = document.getElementById("message");
 
-btn.addEventListener("click", function () {
+const musicBtn = document.getElementById("musicBtn");
+const bgMusic = document.getElementById("bgMusic");
+
+let musicPlayed = false;
+
+btn.addEventListener("click", function(){
 
     clickCount++;
 
-    if (clickCount === 1) {
-        btn.style.transform = "translateX(200px)";
+    // Klik pertama
+    if(clickCount === 1){
+        btn.style.transform = "translateX(220px)";
         message.innerText = "Eits, belum bisa 🤭";
     }
 
-    else if (clickCount === 2) {
-        btn.style.transform = "translateX(-200px)";
-        message.innerText = "Sebelum lanjut, nyalain musik dulu ya 🎵";
+    // Klik kedua
+    else if(clickCount === 2){
+
+        // Kalau musik belum diputar
+        if(!musicPlayed){
+            btn.style.transform = "translateX(-220px)";
+            message.innerText = "Pencet musiknya dulu ya 🎵";
+            musicBtn.style.display = "inline-block";
+
+            // Supaya hitungan klik tidak bertambah
+            clickCount--;
+        }
     }
 
-    else if (clickCount === 3) {
-        btn.style.transform = "translateX(100px)";
+    // Klik ketiga
+    else if(clickCount === 3){
+        btn.style.transform = "translateX(120px)";
         message.innerText = "Hehehe sabar ya bestie 🤭";
     }
 
-    else {
+    // Klik keempat
+    else{
         message.innerText = "Lanjut ke puzzle 🧩";
+
+        // nanti diganti:
+        // window.location.href = "puzzle.html";
     }
+
+});
+
+musicBtn.addEventListener("click", function(){
+
+    bgMusic.play();
+
+    musicPlayed = true;
+
+    musicBtn.style.display = "none";
+
+    message.innerText = "Nah gitu dong, sekarang lanjut 😌✨";
+
 });
