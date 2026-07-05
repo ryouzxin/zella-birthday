f77f8d1d3a3ef909f59b0e47ce6b76da2b88efa3
Tarisa <ryouzxin@gmail.com>
2026-07-05 07:26:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+document.addEventListener('click', function(e){
 
-document.addEventListener('click',e=>{
- const r=document.createElement('div');
- r.className='ripple';
- r.style.left=e.clientX+'px';
- r.style.top=e.clientY+'px';
- document.body.appendChild(r);
- setTimeout(()=>r.remove(),1000);
+    if(e.target.tagName === "BUTTON"){
+        return;
+    }
+
+    const r = document.createElement('div');
+    r.className = 'ripple';
+
+    r.style.left = e.clientX + 'px';
+    r.style.top = e.clientY + 'px';
+
+    document.body.appendChild(r);
+
+    setTimeout(()=>{
+        r.remove();
+    },1000);
+
 });
