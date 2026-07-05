99811101391351820fb4399efcba1a33a216f7ec
Tarisa <ryouzxin@gmail.com>
2026-07-05 07:41:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let clickCount = 0;
 const btn = document.getElementById("openBtn");
+const message = document.getElementById("message");
 
 btn.addEventListener("click", function () {
 
@@ -7,20 +8,20 @@ btn.addEventListener("click", function () {
 
     if (clickCount === 1) {
         btn.style.transform = "translateX(200px)";
-        alert("Eits, belum bisa 🤭");
+        message.innerText = "Eits, belum bisa 🤭";
     }
 
     else if (clickCount === 2) {
         btn.style.transform = "translateX(-200px)";
-        alert("Sebelum lanjut, nyalain musik dulu ya 🎵");
+        message.innerText = "Sebelum lanjut, nyalain musik dulu ya 🎵";
     }
 
     else if (clickCount === 3) {
         btn.style.transform = "translateX(100px)";
-        alert("Hehehe sabar ya bestie 🤭");
+        message.innerText = "Hehehe sabar ya bestie 🤭";
     }
 
     else {
-alert("Lanjut ke puzzle 🧩");
+        message.innerText = "Lanjut ke puzzle 🧩";
     }
 });
